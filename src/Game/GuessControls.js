14aa579032f8e2360d5d5aeab56f3abf6d2905c4
Mitch Faber <faberskits@gmail.com>
2021-11-02 13:34:59 -0300

    Guess binding working now. Guess validation is next.

--- src/Game/GuessControls.js
+++ src/Game/GuessControls.js
@@ -1,19 +1,52 @@
 import React, { Component, useState, useEffect } from 'react'
 
 export default function GuessControls() {
-    let guess1 = 0, guess2 = 0,guess3 = 0
+    const [guess1,setGuess1] = useState(0)
+    const [guess2, setGuess2] = useState(0)
+    const [guess3, setGuess3] = useState(0)
+    let guessCount = 0
     
+    useEffect(() => {
+        //Generate fermi numbers on first run.
+        genNums()
+    }, [])
     
-    let myVar
-    useEffect((e) => {
-        console.log(e)
-        // if (guess1 != null && guess2 != null && guess3 != null) {
-        //     switch()
-        // }
-    },[guess1,guess2,guess3])
+    function genNums() {
+        
+    }
+    
+    function handleNumChange(e) {
+        console.log(e.target.id)
+        switch (e.target.id) {
+            case "num1":
+                setGuess1(e.target.value)
+                break;
+            case "num2":
+                setGuess2(e.target.value)
+                break;
+            case "num3":
+                setGuess3(e.target.value)
+                break;
+            default:
+                // No num change? Not sure what to do here really... 
+                console.log("You must be a wizard or something...")
+        }
+    }
 
     function guess() {
-        console.log("guess!")
+        guessCount++
+        console.log("num 1: " + guess1)
+        console.log("num 2: " + guess2)
+        console.log("num 3: " + guess3)
+        console.log("Guess Count: " + guessCount)
+    }
+
+    function reset() {
+        guessCount = 0
+        setGuess1(0)
+        setGuess2(0)
+        setGuess3(0)
+        genNums()
     }
     
     return (
@@ -21,19 +54,19 @@ export default function GuessControls() {
             <div className="field">
                 <label className="label">1st number</label>
                 <div className="control">
-                    <input type="number" className="input is-primary" value={guess1}></input>
+                    <input type="number" className="input is-primary" onChange={handleNumChange} id="num1" value={guess1}></input>
                 </div>
             </div>
             <div className="field">
                 <label className="label">2nd number</label>
                 <div className="control">
-                    <input type="number" className="input is-primary" value={guess2}></input>
+                    <input type="number" className="input is-primary" onChange={handleNumChange} id="num2" value={guess2}></input>
                 </div>
             </div>
             <div className="field">
                 <label className="label">3rd number</label>
                 <div className="control">
-                    <input type="number" className="input is-primary" value={guess3}></input>
+                    <input type="number" className="input is-primary" onChange={handleNumChange} id="num3" value={guess3}></input>
                 </div>
             </div>
             <div className="field">
@@ -43,7 +76,7 @@ export default function GuessControls() {
             </div>
             <div className="field">
                 <div className="control">
-                    <button type="button" className="button is-secondary">Reset</button>
+                    <button type="button" onClick={reset} className="button is-secondary">Reset</button>
                 </div>
             </div>
         </div>
--- src/Game/HintsBox.js
+++ src/Game/HintsBox.js
@@ -3,8 +3,10 @@ import React, { Component, useState } from 'react'
 export default function HintsBox() {
     return (
         <div className="box" >
-            <textarea value="Previous Guesses&#013;&#010;============&#013;&#010;" className="textarea is-primary has-fixed-size is-vcentered" readOnly rows="13" cols="20">
-            </textarea>
+            <div className="textarea is-primary has-fixed-size is-vcentered">
+                <p>Previous Guesses<br />============</p><br />
+                {/* TODO: Use 3 divs or p tags for each hint, fermi/pico/nano. Also use a single div for guesses used.*/}
+            </div>
         </div>
     )
 }
