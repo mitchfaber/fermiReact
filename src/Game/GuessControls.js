85e978aecde0f25d3d96d9404cd7201055cc8634
Mitch Faber <faberskits@gmail.com>
2021-11-03 12:22:35 -0300

    Guesses needed to be useState, it was resetting each time setGuess was called.

--- src/Game/GuessControls.js
+++ src/Game/GuessControls.js
@@ -1,31 +1,33 @@
 import React, { Component, useState, useEffect } from 'react'
 
 export default function GuessControls() {
-    const [guess1,setGuess1] = useState(0)
-    const [guess2, setGuess2] = useState(0)
-    const [guess3, setGuess3] = useState(0)
-    let guessCount = 0
+    const [guess1, setGuess1] = useState(1)
+    const [guess2, setGuess2] = useState(1)
+    const [guess3, setGuess3] = useState(1)
+    const [guessCount, setGuessCount] = useState(0)
     
     useEffect(() => {
         //Generate fermi numbers on first run.
+        console.log("USE EFFECT")
         genNums()
     }, [])
     
     function genNums() {
-        
+        console.log("GEN NUMS")
+        setGuessCount(0)
     }
     
     function handleNumChange(e) {
-        console.log(e.target.id)
+        let newValue = e.target.value
         switch (e.target.id) {
             case "num1":
-                setGuess1(e.target.value)
+                setGuess1(newValue)
                 break;
             case "num2":
-                setGuess2(e.target.value)
+                setGuess2(newValue)
                 break;
             case "num3":
-                setGuess3(e.target.value)
+                setGuess3(newValue)
                 break;
             default:
                 // No num change? Not sure what to do here really... 
@@ -33,19 +35,28 @@ export default function GuessControls() {
         }
     }
 
+    function validate() {
+        if ((guess1 > 10 || guess2 > 10 || guess3 > 10) || (guess1 < 1 || guess2 < 1 || guess3 < 1))  {
+            return false
+        } else {
+            return true
+        }
+    }
+
     function guess() {
-        guessCount++
-        console.log("num 1: " + guess1)
-        console.log("num 2: " + guess2)
-        console.log("num 3: " + guess3)
-        console.log("Guess Count: " + guessCount)
+        let valid = validate()
+        if (valid) {
+            setGuessCount(guessCount + 1)
+        }
+        console.log(guessCount)
     }
 
     function reset() {
-        guessCount = 0
-        setGuess1(0)
-        setGuess2(0)
-        setGuess3(0)
+        console.log("RESET")
+        setGuessCount(0)
+        setGuess1(1)
+        setGuess2(1)
+        setGuess3(1)
         genNums()
     }
     
@@ -54,19 +65,19 @@ export default function GuessControls() {
             <div className="field">
                 <label className="label">1st number</label>
                 <div className="control">
-                    <input type="number" className="input is-primary" onChange={handleNumChange} id="num1" value={guess1}></input>
+                    <input type="number" className="input is-primary" onChange={handleNumChange} id="num1" value={guess1} min="1" max="10"></input>
                 </div>
             </div>
             <div className="field">
                 <label className="label">2nd number</label>
                 <div className="control">
-                    <input type="number" className="input is-primary" onChange={handleNumChange} id="num2" value={guess2}></input>
+                    <input type="number" className="input is-primary" onChange={handleNumChange} id="num2" value={guess2} min="1" max="10"></input>
                 </div>
             </div>
             <div className="field">
                 <label className="label">3rd number</label>
                 <div className="control">
-                    <input type="number" className="input is-primary" onChange={handleNumChange} id="num3" value={guess3}></input>
+                    <input type="number" className="input is-primary" onChange={handleNumChange} id="num3" value={guess3} min="1" max="10"></input>
                 </div>
             </div>
             <div className="field">
