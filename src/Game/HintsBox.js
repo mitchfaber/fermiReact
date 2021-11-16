51695a5a89a965b38699bb185a45a42e788eb248
Mitch Faber <faberskits@gmail.com>
2021-11-16 15:06:24 -0400

    bug fixing and shuffling hint

--- src/Game/HintsBox.js
+++ src/Game/HintsBox.js
@@ -1,17 +1,17 @@
-import React, { Component, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 
-const LOCAL_STORAGE_KEY = 'fermiGame.Hint'
-export default function HintsBox() {
-    const storedHint = (localStorage.getItem(LOCAL_STORAGE_KEY))
+export default function HintsBox({ hint, guessCount }) {
+    const [prevGuess, setPrevGuess] = useState([{hint : '',count : 0}])
     useEffect(() => {
-        console.log('hintbox hint changed: ' + storedHint)
-    }, [localStorage.getItem(LOCAL_STORAGE_KEY)])
+        console.log("Hint changed I'm in hint box! " + hint)
+        
+    }, [hint])
 
     return (
         <div className="box" >
             <div className="textarea is-primary has-fixed-size is-vcentered">
                 <p>Previous Guesses<br />============</p><br />
-                {/* TODO: Use 3 divs or p tags for each hint, fermi/pico/nano. Also use a single div for guesses used.*/}
+                {hint[1] > 0 ? <div>Guess #{hint[1]} : {hint[0]}</div> : <div></div>}
             </div>
         </div>
     )
